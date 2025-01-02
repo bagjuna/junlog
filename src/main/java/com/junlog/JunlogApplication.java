9ed1ddcf6da4c88002312d32f02690547eb7217f
박준아 <juna3437@naver.com>
2025-01-03 02:37:15 +0900

    first commit

---
+++ src/main/java/com/junlog/JunlogApplication.java
@@ -0,0 +1,13 @@
+package com.junlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunlogApplication.class, args);
+    }
+
+}
