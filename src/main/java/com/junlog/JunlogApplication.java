3f3d5c6c821a8d50475e191790f3e3da5dbad7ac
juna <juna3437@naver.com>
2025-01-14 18:51:26 +0900

    hello world1

--- src/main/java/com/junlog/JunlogApplication.java
+++ src/main/java/com/junlog/JunlogApplication.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// 하하하
 @Slf4j
 @SpringBootApplication
 public class JunlogApplication {
